b21b4264fb50bb0330155cf8f80c91ed51ee1e9f
gowtham-ggg <gowthamvinoth2015@gmail.com>
2024-12-21 07:59:08 +0530

    updated

---
+++ realestate/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./index.css"
+import Header from './components/Header'
+const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
+// --------------------------------------------58:00---------------
